dcad248d6d026bfe520f70d5d272ef9f42b83b5f
Shao Eric Hu <erichu11364@gmail.com>
2018-07-07 19:27:57 -0400

    changed something

--- client/screens/TabScreen.js
+++ client/screens/TabScreen.js
@@ -17,7 +17,7 @@ export default class TabScreen extends React.Component {
       for (let row of Object.keys(result)) {
         list.push(result[row]);
       }
-      console.log(list);
+      //console.log(list);
       this.setState({ items: list });
     });
   }
