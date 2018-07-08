1447c40025b17afa0ea56b0050908648338f7493
Xin Yuan <sheanyxy@gmail.com>
2018-07-08 03:59:18 -0400

    add findAllTransactions

--- client/functions/index.js
+++ client/functions/index.js
@@ -1,6 +1,9 @@
 const functions = require('firebase-functions');
 var stripe = require("stripe")("sk_test_SWu8uCLLjcfBjtm7ZuANmq24");
 var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+const username = encodeURIComponent('firebase')
+const password = encodeURIComponent('a1s2d3f4')
+var url = 'mongodb://'+username+':'+password+'@ds229621.mlab.com:29621/tabulate_transactions';
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,23 +14,22 @@ var MongoClient = require('mongodb').MongoClient, assert = require('assert');
 
 exports.payForDrinks = functions.https.onRequest((request,response) => {
   // Use connect method to connect to the server
-  const token = "tok_1ClW8BF0bNLd1tiiUfdWKOYr"; // Using Express
-  const amount = 500
+  const token = request.body.stripeToken; // Using Express
+  const amount = request.body.amount
+  const user = request.body.user
   const charge = stripe.charges.create({
     amount: amount,
     currency: 'usd',
-    description: 'Example charge',
+    description: 'Charge for drinks',
     source: token,
   }).then((charge)=>{
-    const username = encodeURIComponent('firebase')
-    const password = encodeURIComponent('a1s2d3f4')
-    var url = 'mongodb://'+username+':'+password+'@ds229621.mlab.com:29621/tabulate_transactions';
+
     MongoClient.connect(url, (err, client) => {
       if (err) throw err
       let db = client.db('tabulate_transactions')
       console.log("connected to serversss")
 
-      db.collection('transactions').insertOne({user: 'David Margolin', amount: 555, date: new Date()}, (err,r)=>{
+      db.collection('transactions').insertOne({user: user, amount: amount, date: new Date()}, (err,r)=>{
         console.log("kill me please")
         if (err) throw err
         console.log("success!!!")
@@ -41,5 +43,23 @@ exports.payForDrinks = functions.https.onRequest((request,response) => {
 
   })
 
+
+  })
+
+
+  exports.findAllTransactions = functions.https.onRequest((request,response) => {
+
+    MongoClient.connect(url, (err, client) => {
+      const user = request.body.user;
+      if (err) throw err;
+      var db = client.db("tabulate_transactions");
+      //Find all documents in the customers collection:
+      db.collection("transactions").find({ user: RegExp(user)}).toArray((err, result) => {
+        if (err) throw err;
+        console.log(result);
+        client.close();
+        return response.send(JSON.stringify(result))
+      });
+    });
 
   })
